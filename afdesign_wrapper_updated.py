75e71f6945d68279958a22791844c34361139c19
boolean-jiang <bwjiang@Bowser.localdomain>
2022-09-14 13:42:47 -0700

    afdesign_wrapper_updated now outputs pdb and fasta files

--- afdesign_wrapper_updated.py
+++ afdesign_wrapper_updated.py
@@ -19,8 +19,7 @@ def main():
     parser.add_argument("--logits", help="number of logit optimization steps", type=int, default=100)
     parser.add_argument("--soft", help="number of softmax(logits) optimization steps", type=int, default=100)
     parser.add_argument("--hard", help="number of one_hot(logits) optimization steps", type=int, default=10)
-    parser.add_argument("--pdb_out_name", help="filename of design PDB structure", type=str)
-    #parser.add_argument("--fas_out_name", help="filename of design FASTA file", type=str) 
+    parser.add_argument("--out_name", help="filename of design PDB structure/FASTA file", type=str)
 
     args = parser.parse_args()
 
@@ -39,8 +38,6 @@ def main():
 
     model.restart(opt=opt, weights=weights)
 
-    print("Script actually startingggg")
-
     #Add ability to disable residues, set seq, etc. with CLI arguments later 
 
     #accept CLI arguments for num_recycles later 
@@ -49,11 +46,9 @@ def main():
     else:
         model.design_3stage(args.logits, args.soft, args.hard, num_recycles=int(args.use_multimer>0))
     
-    model.save_pdb(f"{args.pdb_out_name}.pdb")
+    model.save_pdb(f"{args.out_name}.pdb")
 
-    #For now, just return PDB file - later, figure out how to make both FASTA and PDB
-    #with open(f"{args.fas_out_name}.fasta", "w") as outfas: outfas.write(model.get_seqs())
-    #return f"{args.pdb_out_name}.pdb", ####f"{args.fas_out_name}.fasta", 
+    with open(f"{args.out_name}.fasta", "w") as outfas: outfas.write(model.get_seqs())
 
 if __name__ == "__main__":
     main()
